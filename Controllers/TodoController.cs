644f38f2340927916d01a556702b145e9202a568
CaioProg <caioprog18@gmail.com>
2022-08-29 21:11:42 -0300

    Lendo os itens do banco

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,27 @@
+﻿using MeuTodo.Data;
+using MeuTodo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MeuTodo.Controllers
+{
+    [ApiController]
+    [Route(template:"v1")]
+    public class TodoController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route(template:"todos")]
+        public async Task<IActionResult> Get(
+            [FromServices] AddDbContext context)
+        {
+            var todos = await context
+                .Todos
+                .AsNoTracking()
+                .ToListAsync();
+            return Ok(todos);
+        }
+    }
+}
